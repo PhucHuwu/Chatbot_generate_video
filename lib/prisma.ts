299f36bcfc668ac9fe437818d50c7dd891ea8406
PhucHuwu <phuctranhuu37@gmail.com>
2025-11-26 18:12:32 +0700

    feat: add datasource configuration for PrismaClient initialization

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,6 +6,13 @@ export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
         log: ["query"],
+        datasources: {
+            db: {
+                url: process.env.MONGODB_URI?.includes(".mongodb.net/?")
+                    ? process.env.MONGODB_URI.replace(".mongodb.net/?", ".mongodb.net/chatbot-db?")
+                    : process.env.MONGODB_URI,
+            },
+        },
     });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
